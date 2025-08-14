5142337e1b37ea17b9c8b45b61872ef81f992380
Oleksii Muntian <lex.19.mov@gmai.com>
2025-08-14 12:24:15 +0300

    fix: Fixed auth callback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Opcodes\LogViewer\Facades\LogViewer;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        LogViewer::auth(function ($request) {
-            return !is_null($request->user());
-        });
+        Gate::define('viewLogViewer', fn ($user) => !is_null($user));
     }
 }
